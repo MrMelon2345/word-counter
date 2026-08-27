0cb95685a9d4557601b173c9d4dff8f6265f8b0f
MrMelon2345 <alexh2oman09@gmail.com>
2026-08-27 17:11:32 +0100

    Add files via upload

---
+++ website/app.py
@@ -0,0 +1,23 @@
+import re
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    words = None
+
+    if request.method == "POST":
+        text = request.form["text"]
+
+        # Remove text inside quotation marks
+        text = re.sub(r'"[^"]*"', '', text)
+
+        # Count the words
+        words = len(text.split())
+
+    return render_template("index.html", words=words)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
